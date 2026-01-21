51cd111a45ea6627a757252015432f80352d7e75
Rias Gremory <153193656+RiasGremoryHSDXD@users.noreply.github.com>
2026-01-21 22:39:57 +0800

    fix(ui): add LeftSideBar to Schema and Template pages

--- src/pages/Template/template.tsx
+++ src/pages/Template/template.tsx
@@ -1,9 +1,11 @@
 import SideBar from "../../components/SideBar"
+import LeftSideBar from "../../components/leftSideBar/LeftSideBar"
 
 export default function template() {
     return (
-        <div>
+        <div className="flex flex-row justify-between">
             <SideBar pageName="Template" />
+            <LeftSideBar />
         </div>
     )
 }
